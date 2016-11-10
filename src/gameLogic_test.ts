7305fb3e1bd0b617b5febfb6729d8f4b05a55ede
ilanasufrin <ilanasufrin@gmail.com>
2016-11-09 21:53:35 -0500

    all tests for gameLogic

--- src/gameLogic_test.ts
+++ src/gameLogic_test.ts
@@ -1,207 +1,4 @@
-// describe("In TicTacToe", function() {
-//   let OK = true;
-//   let ILLEGAL = false;
-//   let X_TURN = 0;
-//   let O_TURN = 1;
-//   let NO_ONE_TURN = -1;
-//   let NO_ONE_WINS: number[] = null;
-//   let X_WIN_SCORES = [1, 0];
-//   let O_WIN_SCORES = [0, 1];
-//   let TIE_SCORES = [0, 0];
-
-//   function expectStateTransition(
-//         isOk: boolean, stateTransition: IStateTransition) {
-//     if (isOk) {
-//       gameLogic.checkMoveOk(stateTransition);
-//     } else {
-//       // We expect an exception to be thrown :)
-//       let didThrowException = false;
-//       try {
-//         gameLogic.checkMoveOk(stateTransition);
-//       } catch (e) {
-//         didThrowException = true;
-//       }
-//       if (!didThrowException) {
-//         throw new Error("We expect an illegal move, but checkMoveOk didn't throw any exception!")
-//       }
-//     }
-//   }
-
-//   function expectMove(
-//       isOk: boolean,
-//       turnIndexBeforeMove: number,
-//       boardBeforeMove: Board,
-//       row: number,
-//       col: number,
-//       boardAfterMove: Board,
-//       turnIndexAfterMove: number,
-//       endMatchScores: number[]): void {
-//     let stateTransition: IStateTransition = {
-//       turnIndexBeforeMove: turnIndexBeforeMove,
-//       stateBeforeMove: boardBeforeMove ? {board: boardBeforeMove, delta: null} : null,
-//       move: {
-//         turnIndexAfterMove: turnIndexAfterMove,
-//         endMatchScores: endMatchScores,
-//         stateAfterMove: {board: boardAfterMove, delta: {row: row, col: col}}
-//       },
-//       numberOfPlayers: null
-//     };
-//     expectStateTransition(isOk, stateTransition);
-//   }
-
-//   it("Initial move", function() {
-//     expectStateTransition(OK, {
-//       turnIndexBeforeMove: X_TURN,
-//       stateBeforeMove: null,
-//       move: {
-//         turnIndexAfterMove: X_TURN,
-//         endMatchScores: NO_ONE_WINS,
-//         stateAfterMove: {board:
-//           [['', '', ''],
-//           ['', '', ''],
-//           ['', '', '']], delta: null}
-//       },
-//       numberOfPlayers: null
-//     });
-//   });
-
-//   it("Initial move setting turn to O player is illegal", function() {
-//     expectStateTransition(ILLEGAL, {
-//       turnIndexBeforeMove: X_TURN,
-//       stateBeforeMove: null,
-//       move: {
-//         turnIndexAfterMove: O_TURN,
-//         endMatchScores: NO_ONE_WINS,
-//         stateAfterMove: {board:
-//           [['', '', ''],
-//           ['', '', ''],
-//           ['', '', '']], delta: null}
-//       },
-//       numberOfPlayers: null
-//     });
-//   });
-
-//   it("placing X in 0x0 from initial state is legal", function() {
-//     expectMove(OK, X_TURN, null, 0, 0,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], O_TURN, NO_ONE_WINS);
-//   });
-
-//   it("placing X in 0x0 from initial state but setting the turn to yourself is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN, null, 0, 0,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], X_TURN, NO_ONE_WINS);
-//   });
-
-//   it("placing X in 0x0 from initial state and winning is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN, null, 0, 0,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], NO_ONE_TURN, X_WIN_SCORES);
-//   });
-
-//   it("placing X in 0x0 from initial state and setting the wrong board is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN, null, 0, 0,
-//       [['X', 'X', ''],
-//        ['', '', ''],
-//        ['', '', '']], O_TURN, NO_ONE_WINS);
-//   });
-
-//   it("placing O in 0x1 after X placed X in 0x0 is legal", function() {
-//     expectMove(OK, O_TURN,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], 0, 1,
-//       [['X', 'O', ''],
-//        ['', '', ''],
-//        ['', '', '']], X_TURN, NO_ONE_WINS);
-//   });
-
-//   it("placing an O in a non-empty position is illegal", function() {
-//     expectMove(ILLEGAL, O_TURN,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], 0, 0,
-//       [['O', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], X_TURN, NO_ONE_WINS);
-//   });
-
-//   it("cannot move after the game is over", function() {
-//     expectMove(ILLEGAL, O_TURN,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['X', '', '']], 2, 1,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['X', 'O', '']], X_TURN, NO_ONE_WINS);
-//   });
-
-//   it("placing O in 2x1 is legal", function() {
-//     expectMove(OK, O_TURN,
-//       [['O', 'X', ''],
-//        ['X', 'O', ''],
-//        ['X', '', '']], 2, 1,
-//       [['O', 'X', ''],
-//        ['X', 'O', ''],
-//        ['X', 'O', '']], X_TURN, NO_ONE_WINS);
-//   });
-
-//   it("X wins by placing X in 2x0 is legal", function() {
-//     expectMove(OK, X_TURN,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['', '', '']], 2, 0,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['X', '', '']], NO_ONE_TURN, X_WIN_SCORES);
-//   });
-
-//   it("O wins by placing O in 1x1 is legal", function() {
-//     expectMove(OK, O_TURN,
-//       [['X', 'X', 'O'],
-//        ['X', '', ''],
-//        ['O', '', '']], 1, 1,
-//       [['X', 'X', 'O'],
-//        ['X', 'O', ''],
-//        ['O', '', '']], NO_ONE_TURN, O_WIN_SCORES);
-//   });
-
-//   it("the game ties when there are no more empty cells", function() {
-//     expectMove(OK, X_TURN,
-//       [['X', 'O', 'X'],
-//        ['X', 'O', 'O'],
-//        ['O', 'X', '']], 2, 2,
-//       [['X', 'O', 'X'],
-//        ['X', 'O', 'O'],
-//        ['O', 'X', 'X']], NO_ONE_TURN, TIE_SCORES);
-//   });
-
-//   it("move without board is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN,
-//       [['X', 'O', 'X'],
-//        ['X', 'O', 'O'],
-//        ['O', 'X', '']], 2, 2,
-//       null, NO_ONE_TURN, TIE_SCORES);
-//   });
-
-//   it("placing X outside the board (in 0x3) is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN,
-//       [['', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], 0, 3,
-//       [['', '', '', 'X'],
-//        ['', '', ''],
-//        ['', '', '']], O_TURN, NO_ONE_WINS);
-//   });
-// });
-
 describe('Simon gameLogic', function() {
-  it('expects true to be false', function() {
-    expect(false).toBe(true);
-  });
 
   describe('createInitialMove', function() {
     let initialMove: IMove;
@@ -321,34 +118,102 @@ describe('Simon gameLogic', function() {
     let stateBeforeMove: IState;
     let color: number;
     let turnIndexBeforeMove: number;
+    let delta: SequenceDelta = 3;
 
-    // it('adds one to color to the sequence', function() {
-    //   stateBeforeMove = {
-    //     expectedSequence: [3, 0],
-    //     playerSequence: [],
-    //     delta: jasmine.any(Number)
-    //   }
+    it('does not set endMatchScores if there is no winner', function() {
+      stateBeforeMove = {
+        expectedSequence: [3, 0],
+        playerSequence: [],
+        delta: delta
+      }
 
-    //   color: 3;
+      color: 3;
 
-    //   turnIndexBeforeMove: 0;
+      turnIndexBeforeMove: 0;
 
-    //   // expect(currentState.expectedSequence.length).toEqual(3);
-    // });
-    //  it('calls getInitialState if stateBeforeMove is falsy', function() {
-    //   spyOn(gameLogic, 'getInitialState');
+      let answer = gameLogic.createMove(stateBeforeMove, color, turnIndexBeforeMove);
+      expect(answer).toEqual(
+        {
+          endMatchScores: [ 0, 0 ],
+          turnIndexAfterMove: -1,
+          stateAfterMove: {
+            delta: undefined,
+            playerSequence: [ undefined ],
+            expectedSequence: [ 3, 0 ]
+          }
+        }
+       );
+    });
 
-    //   console.debug('!!!', gameLogic.getInitialState);
-    //   console.debug('!!!', gameLogic);
+    it('sets endMatchScores if there is no winner this round', function() {
+       stateBeforeMove = {
+        expectedSequence: [2, 1],
+        playerSequence: [],
+        delta: delta
+      }
 
-    //   stateBeforeMove = undefined;
-    //   color: 3;
-    //   turnIndexBeforeMove: 0;
+      color: 3;
 
-    //   gameLogic.createMove(undefined, color, turnIndexBeforeMove);
+      turnIndexBeforeMove: 0;
 
-    //   expect(gameLogic.getInitialState).toHaveBeenCalled();
-    // });
+      let answer = gameLogic.createMove(stateBeforeMove, color, turnIndexBeforeMove);
+      expect(answer).toEqual(
+        {
+          endMatchScores: [ 0, 0 ],
+          turnIndexAfterMove: -1,
+          stateAfterMove: {
+            delta: undefined,
+            playerSequence: [ undefined ],
+            expectedSequence: [ 2, 1 ]
+          }
+        }
+       );
+    });
+
+    it('clears the playerSequence and adds to the expectedSequence after a full successful turn', function() {
+      stateBeforeMove = {
+        expectedSequence: [2, 3],
+        playerSequence: [ 2 ],
+        delta: delta
+      }
+
+      color: 3;
+
+      turnIndexBeforeMove: 0;
+
+      let answer = gameLogic.createMove(stateBeforeMove, 3, turnIndexBeforeMove);
+
+      expect(answer.stateAfterMove.playerSequence.length).toBe(0);
+      expect(answer.stateAfterMove.expectedSequence.length).toBe(3);
+    });
+
+    it('if stateBeforeMove is falsy it calls getInitialState', function() {
+      stateBeforeMove = null;
+
+      color: 3;
+
+      turnIndexBeforeMove: 0;
+
+      let answer = gameLogic.createMove(stateBeforeMove, color, turnIndexBeforeMove);
+      expect(answer).toEqual(
+        {
+          endMatchScores: [ 0, 0 ],
+          turnIndexAfterMove: -1,
+          stateAfterMove: {
+            delta: undefined,
+            playerSequence: [ undefined ],
+            expectedSequence: [ jasmine.any(Number) ]
+          }
+        }
+       );
+    });
   });
 
+  describe('checkMoveOk', function() {
+    let stateTransition : IStateTransition;
+    it('does nothing at the moment', function() {
+      gameLogic.checkMoveOk(stateTransition);
+      expect(true).toBe(true); //this is obviously just a placeholder
+    });
+  });
 });
--- ts_output_readonly_do_NOT_change_manually/src/gameLogic_test.js
+++ ts_output_readonly_do_NOT_change_manually/src/gameLogic_test.js
@@ -1,189 +1,4 @@
-// describe("In TicTacToe", function() {
-//   let OK = true;
-//   let ILLEGAL = false;
-//   let X_TURN = 0;
-//   let O_TURN = 1;
-//   let NO_ONE_TURN = -1;
-//   let NO_ONE_WINS: number[] = null;
-//   let X_WIN_SCORES = [1, 0];
-//   let O_WIN_SCORES = [0, 1];
-//   let TIE_SCORES = [0, 0];
-//   function expectStateTransition(
-//         isOk: boolean, stateTransition: IStateTransition) {
-//     if (isOk) {
-//       gameLogic.checkMoveOk(stateTransition);
-//     } else {
-//       // We expect an exception to be thrown :)
-//       let didThrowException = false;
-//       try {
-//         gameLogic.checkMoveOk(stateTransition);
-//       } catch (e) {
-//         didThrowException = true;
-//       }
-//       if (!didThrowException) {
-//         throw new Error("We expect an illegal move, but checkMoveOk didn't throw any exception!")
-//       }
-//     }
-//   }
-//   function expectMove(
-//       isOk: boolean,
-//       turnIndexBeforeMove: number,
-//       boardBeforeMove: Board,
-//       row: number,
-//       col: number,
-//       boardAfterMove: Board,
-//       turnIndexAfterMove: number,
-//       endMatchScores: number[]): void {
-//     let stateTransition: IStateTransition = {
-//       turnIndexBeforeMove: turnIndexBeforeMove,
-//       stateBeforeMove: boardBeforeMove ? {board: boardBeforeMove, delta: null} : null,
-//       move: {
-//         turnIndexAfterMove: turnIndexAfterMove,
-//         endMatchScores: endMatchScores,
-//         stateAfterMove: {board: boardAfterMove, delta: {row: row, col: col}}
-//       },
-//       numberOfPlayers: null
-//     };
-//     expectStateTransition(isOk, stateTransition);
-//   }
-//   it("Initial move", function() {
-//     expectStateTransition(OK, {
-//       turnIndexBeforeMove: X_TURN,
-//       stateBeforeMove: null,
-//       move: {
-//         turnIndexAfterMove: X_TURN,
-//         endMatchScores: NO_ONE_WINS,
-//         stateAfterMove: {board:
-//           [['', '', ''],
-//           ['', '', ''],
-//           ['', '', '']], delta: null}
-//       },
-//       numberOfPlayers: null
-//     });
-//   });
-//   it("Initial move setting turn to O player is illegal", function() {
-//     expectStateTransition(ILLEGAL, {
-//       turnIndexBeforeMove: X_TURN,
-//       stateBeforeMove: null,
-//       move: {
-//         turnIndexAfterMove: O_TURN,
-//         endMatchScores: NO_ONE_WINS,
-//         stateAfterMove: {board:
-//           [['', '', ''],
-//           ['', '', ''],
-//           ['', '', '']], delta: null}
-//       },
-//       numberOfPlayers: null
-//     });
-//   });
-//   it("placing X in 0x0 from initial state is legal", function() {
-//     expectMove(OK, X_TURN, null, 0, 0,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], O_TURN, NO_ONE_WINS);
-//   });
-//   it("placing X in 0x0 from initial state but setting the turn to yourself is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN, null, 0, 0,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], X_TURN, NO_ONE_WINS);
-//   });
-//   it("placing X in 0x0 from initial state and winning is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN, null, 0, 0,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], NO_ONE_TURN, X_WIN_SCORES);
-//   });
-//   it("placing X in 0x0 from initial state and setting the wrong board is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN, null, 0, 0,
-//       [['X', 'X', ''],
-//        ['', '', ''],
-//        ['', '', '']], O_TURN, NO_ONE_WINS);
-//   });
-//   it("placing O in 0x1 after X placed X in 0x0 is legal", function() {
-//     expectMove(OK, O_TURN,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], 0, 1,
-//       [['X', 'O', ''],
-//        ['', '', ''],
-//        ['', '', '']], X_TURN, NO_ONE_WINS);
-//   });
-//   it("placing an O in a non-empty position is illegal", function() {
-//     expectMove(ILLEGAL, O_TURN,
-//       [['X', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], 0, 0,
-//       [['O', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], X_TURN, NO_ONE_WINS);
-//   });
-//   it("cannot move after the game is over", function() {
-//     expectMove(ILLEGAL, O_TURN,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['X', '', '']], 2, 1,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['X', 'O', '']], X_TURN, NO_ONE_WINS);
-//   });
-//   it("placing O in 2x1 is legal", function() {
-//     expectMove(OK, O_TURN,
-//       [['O', 'X', ''],
-//        ['X', 'O', ''],
-//        ['X', '', '']], 2, 1,
-//       [['O', 'X', ''],
-//        ['X', 'O', ''],
-//        ['X', 'O', '']], X_TURN, NO_ONE_WINS);
-//   });
-//   it("X wins by placing X in 2x0 is legal", function() {
-//     expectMove(OK, X_TURN,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['', '', '']], 2, 0,
-//       [['X', 'O', ''],
-//        ['X', 'O', ''],
-//        ['X', '', '']], NO_ONE_TURN, X_WIN_SCORES);
-//   });
-//   it("O wins by placing O in 1x1 is legal", function() {
-//     expectMove(OK, O_TURN,
-//       [['X', 'X', 'O'],
-//        ['X', '', ''],
-//        ['O', '', '']], 1, 1,
-//       [['X', 'X', 'O'],
-//        ['X', 'O', ''],
-//        ['O', '', '']], NO_ONE_TURN, O_WIN_SCORES);
-//   });
-//   it("the game ties when there are no more empty cells", function() {
-//     expectMove(OK, X_TURN,
-//       [['X', 'O', 'X'],
-//        ['X', 'O', 'O'],
-//        ['O', 'X', '']], 2, 2,
-//       [['X', 'O', 'X'],
-//        ['X', 'O', 'O'],
-//        ['O', 'X', 'X']], NO_ONE_TURN, TIE_SCORES);
-//   });
-//   it("move without board is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN,
-//       [['X', 'O', 'X'],
-//        ['X', 'O', 'O'],
-//        ['O', 'X', '']], 2, 2,
-//       null, NO_ONE_TURN, TIE_SCORES);
-//   });
-//   it("placing X outside the board (in 0x3) is illegal", function() {
-//     expectMove(ILLEGAL, X_TURN,
-//       [['', '', ''],
-//        ['', '', ''],
-//        ['', '', '']], 0, 3,
-//       [['', '', '', 'X'],
-//        ['', '', ''],
-//        ['', '', '']], O_TURN, NO_ONE_WINS);
-//   });
-// });
 describe('Simon gameLogic', function () {
-    it('expects true to be false', function () {
-        expect(false).toBe(true);
-    });
     describe('createInitialMove', function () {
         var initialMove;
         it('is blank except for one expected move', function () {
@@ -278,26 +93,79 @@ describe('Simon gameLogic', function () {
         var stateBeforeMove;
         var color;
         var turnIndexBeforeMove;
-        // it('adds one to color to the sequence', function() {
-        //   stateBeforeMove = {
-        //     expectedSequence: [3, 0],
-        //     playerSequence: [],
-        //     delta: jasmine.any(Number)
-        //   }
-        //   color: 3;
-        //   turnIndexBeforeMove: 0;
-        //   // expect(currentState.expectedSequence.length).toEqual(3);
-        // });
-        //  it('calls getInitialState if stateBeforeMove is falsy', function() {
-        //   spyOn(gameLogic, 'getInitialState');
-        //   console.debug('!!!', gameLogic.getInitialState);
-        //   console.debug('!!!', gameLogic);
-        //   stateBeforeMove = undefined;
-        //   color: 3;
-        //   turnIndexBeforeMove: 0;
-        //   gameLogic.createMove(undefined, color, turnIndexBeforeMove);
-        //   expect(gameLogic.getInitialState).toHaveBeenCalled();
-        // });
+        var delta = 3;
+        it('does not set endMatchScores if there is no winner', function () {
+            stateBeforeMove = {
+                expectedSequence: [3, 0],
+                playerSequence: [],
+                delta: delta
+            };
+            color: 3;
+            turnIndexBeforeMove: 0;
+            var answer = gameLogic.createMove(stateBeforeMove, color, turnIndexBeforeMove);
+            expect(answer).toEqual({
+                endMatchScores: [0, 0],
+                turnIndexAfterMove: -1,
+                stateAfterMove: {
+                    delta: undefined,
+                    playerSequence: [undefined],
+                    expectedSequence: [3, 0]
+                }
+            });
+        });
+        it('sets endMatchScores if there is no winner this round', function () {
+            stateBeforeMove = {
+                expectedSequence: [2, 1],
+                playerSequence: [],
+                delta: delta
+            };
+            color: 3;
+            turnIndexBeforeMove: 0;
+            var answer = gameLogic.createMove(stateBeforeMove, color, turnIndexBeforeMove);
+            expect(answer).toEqual({
+                endMatchScores: [0, 0],
+                turnIndexAfterMove: -1,
+                stateAfterMove: {
+                    delta: undefined,
+                    playerSequence: [undefined],
+                    expectedSequence: [2, 1]
+                }
+            });
+        });
+        it('clears the playerSequence and adds to the expectedSequence after a full successful turn', function () {
+            stateBeforeMove = {
+                expectedSequence: [2, 3],
+                playerSequence: [2],
+                delta: delta
+            };
+            color: 3;
+            turnIndexBeforeMove: 0;
+            var answer = gameLogic.createMove(stateBeforeMove, 3, turnIndexBeforeMove);
+            expect(answer.stateAfterMove.playerSequence.length).toBe(0);
+            expect(answer.stateAfterMove.expectedSequence.length).toBe(3);
+        });
+        it('if stateBeforeMove is falsy it calls getInitialState', function () {
+            stateBeforeMove = null;
+            color: 3;
+            turnIndexBeforeMove: 0;
+            var answer = gameLogic.createMove(stateBeforeMove, color, turnIndexBeforeMove);
+            expect(answer).toEqual({
+                endMatchScores: [0, 0],
+                turnIndexAfterMove: -1,
+                stateAfterMove: {
+                    delta: undefined,
+                    playerSequence: [undefined],
+                    expectedSequence: [jasmine.any(Number)]
+                }
+            });
+        });
+    });
+    describe('checkMoveOk', function () {
+        var stateTransition;
+        it('does nothing at the moment', function () {
+            gameLogic.checkMoveOk(stateTransition);
+            expect(true).toBe(true); //this is obviously just a placeholder
+        });
     });
 });
 //# sourceMappingURL=gameLogic_test.js.map
